4b4f04f7d69c74a6f99121f0a8baec127bc12c69
Andrea Colleoni <andrea@colleoni.info>
2019-12-20 14:11:00 +0100

    gg1

---
+++ test.js
@@ -0,0 +1,12 @@
+a = 10;
+b = { nome: 'Andrea'};
+// alert('ciao!!');
+
+function saluta() {
+    alert('questo èun saluto');
+}
+
+function modificaTitolo() {
+    document.getElementById('titolo1').innerHTML 
+        = document.getElementById('txt').value;
+}
