2a395de2afcf94fc0595ad657aaaecf1c0960a67
Andrea Colleoni <andrea@colleoni.info>
2020-02-07 12:52:41 +0100

    posts e users

--- doxi-ng/src/app/post.ts
+++ doxi-ng/src/app/post.ts
@@ -1,5 +1,8 @@
+import { User } from './user';
+
 export class Post {
   userId: number;
+  user: User; // questa prop non c'è nel JSON
   id: number;
   title: string;
   body: string;
--- doxi-ng/src/app/services/posts.service.ts
+++ doxi-ng/src/app/services/posts.service.ts
@@ -1,19 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../post';
-import { Observable } from 'rxjs';
+import { Observable, concat } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { UserService } from './user.service';
+import { User } from '../user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
+  users: User[];
+
   constructor(
     private http: HttpClient,
-  ) { }
+    private userService: UserService,
+  ) {
+    this.userService.getUsers().subscribe(u => this.users = u);
+  }
 
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts').pipe(
+      tap(ps => console.log(ps)),
+      map(ps => {
+        ps.forEach(p => p.user = this.users.find(u => u.id === p.userId));
+        return ps;
+      }),
+      tap(ps => console.log(ps))
+    );
   }
 
 }
+
+
+/*
+{
+  this.userService.getUser(p.userId).subscribe(u => p.user = u);
+}
+*/
